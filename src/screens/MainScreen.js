c39b30521b1ab2443caa5b869a39de99cd59d56a
Andrey <35691656+fufylev@users.noreply.github.com>
2020-03-10 11:16:20 +0300

    The whole App was connected to Google Data Base - FireBase
    The whole App was connected to Google Data Base - FireBase

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { FlatList, StyleSheet, View, Image, Dimensions } from 'react-native';
 import AddTodo from '../components/AddTodo';
@@ -6,11 +6,22 @@ import Todo from '../components/Todo';
 import { THEME } from '../theme';
 import { TodoContext } from '../context/todo/todoContext';
 import { ScreenContext } from '../context/screen/screenContext';
+import { AppLoader } from '../components/ui/AppLoader';
+import AppText from '../components/ui/AppText';
+import AppButton from '../components/ui/AppButton';
 
 const MainScreen = () => {
-    const { addTodo, todos, removeTodo } = useContext(TodoContext);
+    const { addTodo, todos, removeTodo, fetchTodos, loading, error } = useContext(TodoContext);
     const { changeScreen } = useContext(ScreenContext);
     const [deviceWidth, setDeviceWidth] = useState(Dimensions.get('window').width - THEME.PADDING_HORIZONTAL * 2);
+
+    const loadTodos = useCallback(async () => {
+        await fetchTodos();
+    }, [fetchTodos]);
+
+    useEffect(() => {
+        loadTodos();
+    }, []);
 
     useEffect(() => {
         const update = () => {
@@ -24,6 +35,19 @@ const MainScreen = () => {
             Dimensions.removeEventListener('change', update);
         };
     });
+
+    if (loading) {
+        return <AppLoader />;
+    }
+
+    if (error) {
+        return (
+            <View style={styles.center}>
+                <AppText style={styles.error}>{error}</AppText>
+                <AppButton onPress={loadTodos}>Try again</AppButton>
+            </View>
+        );
+    }
 
     let content = (
         <View style={{ width: deviceWidth, flex: 1 }}>
@@ -72,6 +96,16 @@ const styles = StyleSheet.create({
         width: '100%',
         height: '100%',
         resizeMode: 'contain',
+    },
+    center: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    error: {
+        fontSize: 20,
+        color: THEME.DANGER_COLOR,
+        marginBottom: 20
     },
 });
 
